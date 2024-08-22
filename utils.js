da1e0f285a94d41925297cca1422aa246edb0013
nhonyara <c057825@yara.com>
2024-08-22 22:27:30 +0700

    handle UI and script to check balance

--- utils.js
+++ utils.js
@@ -4,6 +4,19 @@ function convertUrlEndpoint(network, address, apiKeyIndex) {
   return `${networkUrl}&address=${address}&apikey=${networkAPIKey}`;
 }
 
-const convertBalance = (result) => {
+function convertBalance(result) {
   return result / Math.pow(10, 18);
-};
+}
+
+function getCorrectWalletList(walletArray) {
+  const correctWallets = [];
+  let count = 0;
+  walletArray.forEach((element, i) => {
+    if (element.length === 42 && element.startsWith("0x") && count < 50) {
+      count += 1;
+      correctWallets.push(element);
+    }
+  });
+
+  return correctWallets;
+}
